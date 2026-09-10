85c15b5c6e5318e410bc53e97e034083b2bb1a22
Oluwafisayomi-P-Folaranmi <folaranmfisayomi@gmail.com>
2026-09-10 15:29:50 +0100

    feat: spring boot banner will not be displayed when application is started

--- 0x02-Spring-Boot/task-manager/src/main/java/com/task_manager/TaskManagerApplication.java
+++ 0x02-Spring-Boot/task-manager/src/main/java/com/task_manager/TaskManagerApplication.java
@@ -1,13 +1,18 @@
 package com.task_manager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class TaskManagerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Application started.");
+    }
 }
